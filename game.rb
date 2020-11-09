6cb3360a6e260f99d6fb1911af458c26fa2d77a6
Aleksandar <aleksandar.dmitrovic@gmail.com>
2020-11-09 12:34:37 -0700

    Create game_over? fn to check if player.lives = 0

--- game.rb
+++ game.rb
@@ -9,7 +9,7 @@ class Game
     @player2 = Player.new
     puts "Welcome #{@player1.name} and #{@player2.name}"
     puts "Let the game begin"
-    @player1_turn = true
+    @player1_turn = false
   end
 
   
@@ -19,7 +19,7 @@ class Game
     @answer = question.answer
     if @player1_turn
       puts "#{@player1.name} #{@output}"
-    elsif @player1_turn
+    else
       puts "#{@player2.name} #{@output}"
     end
   end
@@ -31,11 +31,13 @@ class Game
         puts "P1 #{@player1.lives}/3 vs P2:#{@player2.lives}/3"
         puts "----- NEW TURN -----"
         @player1_turn = false
+        game_over?
       else
         puts "#{@player2.name}: YES! You are correct."
         puts "P1 #{@player1.lives}/3 vs P2:#{@player2.lives}/3"
         puts "----- NEW TURN -----"
         @player1_turn = true
+        game_over?
       end
     else
       if @player1_turn
@@ -44,13 +46,29 @@ class Game
         puts "P1 #{@player1.lives}/3 vs P2:#{@player2.lives}/3"
         puts "----- NEW TURN -----"
         @player1_turn = false
+        game_over?
       else
         puts "#{@player2.name}: Seriously? No!."
         @player2.lose
-        puts "P1 #{@player2.lives}/3 vs P2:#{@player2.lives}/3"
+        puts "P1 #{@player1.lives}/3 vs P2:#{@player2.lives}/3"
         puts "----- NEW TURN -----"
         @player1_turn = true
+        game_over?
       end
+    end
+  end
+
+  def game_over?
+    if @player1.lives == 0
+      puts "#{@player2.name} wins with a score of #{@player2.lives}/3"
+      puts "----- GAME OVER -----"
+      puts " Good bye!"
+    elsif @player2.lives == 0
+      puts "#{@player1.name} wins with a score of #{@player1.lives}/3"
+      puts "----- GAME OVER -----"
+      puts " Good bye!"
+    else
+      start
     end
   end
   
