d71b47aa8a0928fd1a6c186ee72cf77e7b989d12
Aleksandar <aleksandar.dmitrovic@gmail.com>
2020-11-09 12:24:12 -0700

    Define check_response and turn state logic

--- game.rb
+++ game.rb
@@ -9,22 +9,58 @@ class Game
     @player2 = Player.new
     puts "Welcome #{@player1.name} and #{@player2.name}"
     puts "Let the game begin"
+    @player1_turn = true
   end
 
   
-  # def question
-  #   question = Question.new
-  #   @output = question.question
-  #   @answer = question.answer
-  #   puts
+  def question
+    question = Question.new
+    @output = question.question
+    @answer = question.answer
+    if @player1_turn
+      puts "#{@player1.name} #{@output}"
+    elsif @player1_turn
+      puts "#{@player2.name} #{@output}"
+    end
+  end
 
-  # end
+  def check_response
+    if $stdin.gets.chomp.to_i == @answer
+      if @player1_turn
+        puts "#{@player1.name}: YES! You are correct."
+        puts "P1 #{@player1.lives}/3 vs P2:#{@player2.lives}/3"
+        puts "----- NEW TURN -----"
+        @player1_turn = false
+      else
+        puts "#{@player2.name}: YES! You are correct."
+        puts "P1 #{@player1.lives}/3 vs P2:#{@player2.lives}/3"
+        puts "----- NEW TURN -----"
+        @player1_turn = true
+      end
+    else
+      if @player1_turn
+        puts "#{@player1.name}: Seriously? No!."
+        @player1.lose
+        puts "P1 #{@player1.lives}/3 vs P2:#{@player2.lives}/3"
+        puts "----- NEW TURN -----"
+        @player1_turn = false
+      else
+        puts "#{@player2.name}: Seriously? No!."
+        @player2.lose
+        puts "P1 #{@player2.lives}/3 vs P2:#{@player2.lives}/3"
+        puts "----- NEW TURN -----"
+        @player1_turn = true
+      end
+    end
+  end
   
-  # def start
-  #   question
-  # end
+  def start
+    question
+    check_response
+  end
 
 
 end
 
-Game.new
+game = Game.new
+game.start
--- question.rb
+++ question.rb
@@ -6,7 +6,7 @@ class Question
      num1 = rand(1..20)
      num2 = rand(1..20)
      @answer = num1 + num2
-     @question = "What does #{num1} plus #{num2} equal"
+     @question = "What does #{num1} plus #{num2} equal?"
   end
 
 end
